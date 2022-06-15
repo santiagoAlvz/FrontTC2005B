9e0ad6c60413a681a070dc977f5681c424818aef
A01634135 <A01634135@itesm.mx>
2022-06-15 12:52:13 -0500

    Se hizo el diseño de la página principal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,7 +33,7 @@ export default class Home extends Component {
 	render(){
 		return(
 			<StyledHome>
-				<div>
+				<div-home>
 					{this.state.enableAlert ? <Alert message={this.state.alert} type={this.state.alertType}/> : null}
 					<h1>INICIO DE SESIÓN</h1>
 					<EmailEntry ref={email => this.email = email} label="Correo Electrónico"/>
@@ -41,7 +41,7 @@ export default class Home extends Component {
 					<Button onClick={this.login}>Entrar</Button>
 					<LinkButton href="/Registro">¿No estás dentro? Regístrate</LinkButton>
 					{this.state.redirect ? <Navigate to={this.state.route}/> : null }
-				</div>
+				</div-home>
 			</StyledHome>
 		)
 	}
